18754c6ddbcc8a02c8081e4a29b883bfae7c815b
herecomesjaycee <jaycee.cheong04@gmail.com>
2016-12-14 20:24:31 +0000

    Update fizzbuzz.rb and fizzbuzz_spec.rb

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,32 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz'  do
+	it 'returns "fizz" when passed 3' do
+		expect(fizzbuzz(3)).to eq 'fizz'
+	end
+	
+end
+
+describe 'fizzbuzz' do
+	it 'returns "buzz" when passed 5' do
+		expect(fizzbuzz(5)).to eq 'buzz'
+	end
+end
+
+describe 'fizzbuzz' do
+	it 'returns "fizzbuzz" when passed 15' do
+		expect(fizzbuzz(15)).to eq 'fizzbuzz'
+	end
+end
+
+describe 'fizzbuzz' do
+	it 'returns 7 when passed 7' do
+		expect(fizzbuzz(7)).to eq 7
+	end
+end
+
+describe 'fizzbuzz' do
+	it 'return "fizzbuzz" when passed 30' do
+		expect(fizzbuzz(30)).to eq 'fizzbuzz'
+	end
+end
